72a2e9806c3b235a58ec0d44467ccc1d6e3ade0f
Teerrthan118 <94755143+Teerrthan118@users.noreply.github.com>
2022-07-27 09:07:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#969A97');
+
+    textSize(difference);
+    fill('#FFE787');
+    text('Hello!', 50, 400);
+}
